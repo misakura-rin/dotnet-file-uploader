ba4bfce0bb3094ca13f2b2ae6ee3917f77b2c82c
fabia <fabia@DESKTOP-FABI>
2022-07-03 17:47:08 +0200

    Projektdateien hinzufügen.

---
+++ FileUploader/Controllers/UploadController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploader.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadController : Controller
+    {
+        private readonly IOptions _options;
+        public UploadController(IOptions options)
+        {
+            _options = options;
+        }
+
+        [HttpPost]
+        public IActionResult Index(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return StatusCode(StatusCodes.Status403Forbidden);
+            }
+
+            if (_options.Token != token)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden);
+            }
+
+            var file = HttpContext.Request.Form.Files.First();
+            if(file is null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+
+            var extension = Path.GetExtension(file.FileName);
+            
+            if (!_options.AllowedFileExtensions.Contains(extension))
+            {
+                return StatusCode(StatusCodes.Status403Forbidden);
+            }
+
+            var filename = Guid.NewGuid().ToString().Replace("-", "") +  extension;
+
+            var path = Path.Combine(_options.OutputDir, filename);
+
+            using var stream = new FileStream(path, FileMode.Create);
+            file.CopyTo(stream);
+
+            return Ok(Path.Combine(Request.Host.Host, path));
+        }
+    }
+}
---
+++ FileUploader/Options.cs
@@ -0,0 +1,29 @@
+﻿namespace FileUploader
+{
+    public interface IOptions
+    {
+        string Token { get; }
+        string OutputDir { get; set; }
+        IEnumerable<string> AllowedFileExtensions { get; set; }
+    }
+
+    public class Options : IOptions
+    {
+        public string Token { get; }
+        public string OutputDir { get; set; }
+        public IEnumerable<string> AllowedFileExtensions { get; set; }
+
+        public Options()
+        {
+            Token = Environment.GetEnvironmentVariable("API_TOKEN") ?? throw new ArgumentException("Environment does not contain a value for 'API_TOKEN'!");
+            OutputDir = Environment.GetEnvironmentVariable("OUTPUT_DIR") ?? throw new ArgumentException("Environment does not contain a value for 'OUTPUT_DIR'!");
+            AllowedFileExtensions = ParseFileExtensions();
+        }
+
+        private IEnumerable<string> ParseFileExtensions()
+        {
+            var rawExtensions = Environment.GetEnvironmentVariable("ALLOWED_FILE_EXTENSIONS") ?? throw new ArgumentException("Environment does not contain a value for 'ALLOWED_FILE_EXTENSIONS'!");
+            return rawExtensions.Split(",");
+        }
+    }
+}
